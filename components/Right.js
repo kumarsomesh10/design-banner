4c68ca504d2546f5ca6dae75425f5428abac0b19
Somesh Kumar <kumarsomesh002@gmail.com>
2023-08-31 00:34:24 +0530

    Banner Design App

--- components/Right.js
+++ components/Right.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "../styles/Right.module.css";
 
 const right = ({ imageOptions, handleInputData }) => {
